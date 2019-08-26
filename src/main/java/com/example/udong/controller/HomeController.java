b7b041b72eb27a360d5c279eb351bfdef4ac9309
byhero <pjm93love@naver.com>
2019-08-26 17:57:11 +0900

    controller init working

--- src/main/java/com/example/udong/controller/HomeController.java
+++ src/main/java/com/example/udong/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.example.udong.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -11,4 +17,28 @@ public class HomeController {
     public void action() {
     }
 
+    // Receive Parameters from Html Using @RequestParam Map with @PathVariable
+    @RequestMapping(value = "/home/{action}", method = { RequestMethod.GET, RequestMethod.POST })
+    public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
+            ModelAndView modelandView) {
+
+        Object resultMap = new HashMap<String, Object>();
+
+        // divided depending on action value
+        if ("login".equals(action)) {
+            // login logic
+        } else if ("signup".equals(action)) {
+            // sign up logic
+        } else if ("logout".equals(action)) {
+            // logout logic
+        }
+
+        String viewName = "/home/" + action;
+
+        modelandView.setViewName(viewName);
+
+        modelandView.addObject("paramMap", paramMap);
+        modelandView.addObject("resultMap", resultMap);
+        return modelandView;
+    }
 }
