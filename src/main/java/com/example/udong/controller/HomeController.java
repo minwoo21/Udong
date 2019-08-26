d0f9de805bea9cb042918dbf9525a3b31c2aaad3
chaelin1211 <cofls749@naver.com>
2019-08-26 17:19:11 +0900

    commit

--- src/main/java/com/example/udong/controller/HomeController.java
+++ src/main/java/com/example/udong/controller/HomeController.java
@@ -1,8 +1,5 @@
 package com.example.udong.controller;
 
-import com.example.udong.service.StudentService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
