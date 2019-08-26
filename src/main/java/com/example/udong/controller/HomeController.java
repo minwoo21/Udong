50a203dc4c4cf37f58848b4893d6901df0e07b42
chaelin1211 <cofls749@naver.com>
2019-08-26 17:53:21 +0900

    jeon

--- src/main/java/com/example/udong/controller/HomeController.java
+++ src/main/java/com/example/udong/controller/HomeController.java
@@ -1,7 +1,7 @@
 package com.example.udong.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +10,13 @@ public class HomeController {
     @RequestMapping(value = { "/home" })
     public void action() {
     }
-
+    @RequestMapping(value = "/community/{action}")
+    public String action02( @PathVariable String action) {
+        String viewName = "/community/";
+        if("post".equals(action)){
+            viewName += action;
+           
+        } 
+        return viewName;
+    }
 }
