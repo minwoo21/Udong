30f3297cfcd8261bafca44e4cd32c864b395b2f4
chaelin1211 <cofls749@naver.com>
2019-08-27 10:48:19 +0900

    commit

--- src/main/java/com/example/udong/controller/HomeController.java
+++ src/main/java/com/example/udong/controller/HomeController.java
@@ -1,25 +1,53 @@
 package com.example.udong.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.udong.service.MemberService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = { "/home" })
-    public void home_action() {
-    }
-    @RequestMapping(value = { "/support/faq" })
-    public void faq_action() {
-    }
-    @RequestMapping(value = "/community/{action}")
-    public String action02( @PathVariable String action) {
-        String viewName = "/community/";
-        if("post".equals(action)){
-            viewName += action;
+    @Autowired
+    private MemberService service;
 
+    @RequestMapping(value = "/home")
+    public ModelAndView home_action(@RequestParam Map<String, Object> param, @PathVariable String action,
+    ModelAndView modelAndView) {
+        String viewName ="/home";
+        Object resultMap = new Object();
+        Map<String, Object> flagMap = new HashMap<String, Object>();
+        resultMap = service.getMember(param);
+        if(resultMap.equals(null)){
+            flagMap.put("flag", false);
         }
-        return viewName;
+        else{
+            flagMap.put("flag", true);
+            modelAndView.addObject("resultMap", resultMap);
+        }
+        modelAndView.setViewName(viewName);
+        modelAndView.addObject("flag",flagMap);
+        return modelAndView;
     }
+    // @RequestMapping(value = { "/support/faq" })
+    // public void faq_action(@RequestParam Map<String, Object> param, @PathVariable String action,
+    // ModelAndView modelAndView) {
+    // }
+    // @RequestMapping(value = "/community/{action}")
+    // public String action02(@RequestParam Map<String, Object> param, @PathVariable String action,
+    // ModelAndView modelAndView) {
+    //     String viewName = "/community/";
+    //     if("post".equals(action)){
+    //         viewName += action;
+
+    //     }
+    //     return viewName;
+    // }
 }
