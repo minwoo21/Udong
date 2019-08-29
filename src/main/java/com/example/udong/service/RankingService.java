75eb153a679b16e8846749136e3fb8c06465dfb7
byhero <pjm93love@naver.com>
2019-08-29 16:30:50 +0900

    ranking correct

--- src/main/java/com/example/udong/service/RankingService.java
+++ src/main/java/com/example/udong/service/RankingService.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.crypto.Data;
-
 import com.example.udong.repository.RankingDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +19,23 @@ public class RankingService {
         String sqlMapid = "ranking.fetch";
         Object resultObject = dao.getList(sqlMapid, dataMap);
 
+        // Append Rank num
+        ArrayList<HashMap<String, Object>> resultObject02 = new ArrayList<HashMap<String, Object>>();
+
         int recordsnum = ((ArrayList<HashMap<String, Object>>) resultObject).size();
 
-        for (int i = 0; i < recordsnum; i++) {
-            ((Map) ((ArrayList<Object>) resultObject).get(i)).put("RANK", i + 1);
+        if (recordsnum > 10) {
+            for (int i = 0; i < 10; i++) {
+                resultObject02.add((HashMap<String, Object>) ((Map) ((ArrayList<Object>) resultObject).get(i)));
+                resultObject02.get(i).put("RANK", i + 1);
+            }
+        } else {
+            for (int i = 0; i < recordsnum; i++) {
+                // ((Map) ((ArrayList<Object>) resultObject).get(i)).put("RANK", i + 1);
+                resultObject02.add((HashMap<String, Object>) ((Map) ((ArrayList<Object>) resultObject).get(i)));
+                resultObject02.get(i).put("RANK", i + 1);
+            }
         }
-
-        return resultObject;
+        return resultObject02;
     }
 }
