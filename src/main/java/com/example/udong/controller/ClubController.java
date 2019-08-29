468cca36e156e6168627d2fff2930e1e18e6da02
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-30 00:23:30 +0900

    park

--- src/main/java/com/example/udong/controller/ClubController.java
+++ src/main/java/com/example/udong/controller/ClubController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.example.udong.service.ClubService;
+import com.example.udong.service.RankingService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,7 +18,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class ClubController {
 
     @Autowired
-    private ClubService service;
+    private RankingService service;
+    private ClubService service2;
 
     // Receive Parameters from Html Using @RequestParam Map with @PathVariable
     @RequestMapping(value = "/club/{action}", method = { RequestMethod.GET, RequestMethod.POST })
@@ -34,16 +36,16 @@ public class ClubController {
         }
         // divided depending on action value
         if ("location".equals(action)) {
-            resultMap = service.getlocation(paramMap);
+            resultMap = service2.getlocation(paramMap);
             // location logic
         } else if ("location_search".equals(action)) {
-            resultMap = service.getlocation_search(paramMap);
+            resultMap = service2.getlocation_search(paramMap);
             // interest up logic
         } else if ("interest".equals(action)) {
-            resultMap = service.getinterest(paramMap);
+            resultMap = service2.getinterest(paramMap);
             // interest up logic
         } else if ("interest_search".equals(action)) {
-            resultMap = service.getinterest_search(paramMap);
+            resultMap = service2.getinterest_search(paramMap);
             // interest up logic
         } else if ("ranking".equals(action)) {
             // ranking logic
---
+++ src/main/java/com/example/udong/service/RankingService.java
@@ -0,0 +1,46 @@
+package com.example.udong.service;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.udong.repository.ClubRepository;
+import com.example.udong.util.ClubDao;
+import com.example.udong.util.MemberDao;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class RankingService{
+
+    @Autowired
+    private ClubRepository repository;
+    
+    @Autowired
+    private ClubDao dao;
+
+    public Object getlocation(Object dataMap){
+        String sqlMapid = "Club.location";
+        Object resultObject = dao.getList(sqlMapid,dataMap);
+        return resultObject;
+    }
+
+    public Object getlocation_search(Object dataMap){
+        String sqlMapid = "Club.location_search";
+        Object resultObject = dao.getList(sqlMapid,dataMap);
+        return resultObject;
+    }
+    
+    public Object getinterest(Object dataMap){
+        String sqlMapid = "Club.interest";
+        Object resultObject = dao.getList(sqlMapid,dataMap);
+        return resultObject;
+    }
+
+    public Object getinterest_search(Object dataMap){
+        String sqlMapid = "Club.interest_search";
+        Object resultObject = dao.getList(sqlMapid,dataMap);
+        return resultObject;
+    }
+}
